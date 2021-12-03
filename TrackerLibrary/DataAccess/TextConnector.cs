07b6085e4d2a696e58afc9572a4e486341686acf
Federico Jacome <fedeandresdeveloper@gmail.com>
2021-12-03 17:45:22 +0100

    Text file connection

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Model;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            int currentId = 1;
+
+            if(prizes.Count() > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.id).First().id + 1;
+            }
+
+            model.id = currentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
+
             return model;
         }
 
